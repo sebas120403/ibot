beb8659da845b8059d48e8f61bfeb1e25855da0a
iPraiser44 <63963385+iPraiser44@users.noreply.github.com>
2020-04-19 18:25:14 +0300

    Update script.js

--- script.js
+++ script.js
@@ -154,6 +154,7 @@ class Client {
 
     loadGUI() {
         $('.agario-promo-container').replaceWith(`
+        <input onchange="localStorage.setItem('botNick', this.value);" id="botNick" maxlength="15" class="form-control" placeholder="Bot Name" value="fn.tc/ibots" style="display: none;"></input>
         <input onchange="localStorage.setItem('botAmount', this.value);" id="BotAmount" maxlength="3" class="form-control" placeholder="Bot Amount" value="10"></input>
         <center><button id="toggleButton" onclick="window.client.startBots(localStorage.getItem('botAmount'));" class="btn btn-success">Start Bots</button></center>
         `);
